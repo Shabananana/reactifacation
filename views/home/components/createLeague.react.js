6a33ccbf7e733eba2e0b0bbb338362f14faed744
Jeff Davis <jdavis@draftkings.com>
2015-05-15 20:06:37 -0400

    Adding base setup for views folder

---
+++ views/home/components/createLeague.react.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var React = require('react');
+
+var { Component } = React;
+
+class CreateLeague extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+
+    }
+}
+
+module.exports = CreateLeague;
---
+++ views/home/components/league.react.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var React = require('react');
+
+var { Component } = React;
+
+class League extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+
+    }
+}
+
+module.exports = League;
