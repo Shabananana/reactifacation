ef5efd8113916ace792cb2ad5e76db874c1d675f
Jeff Davis <jdavis@draftkings.com>
2015-05-15 20:13:39 -0400

    Continued work on setup

---
+++ views/lib/components/entryGraphic.react.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var React = require('react');
+
+var { Component } = React;
+
+class EntryGraphic extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+
+    }
+}
+
+module.exports = EntryGraphic;
